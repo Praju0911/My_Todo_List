f316b693e3d7df8038fac0a1031d01a2ca1e970c
Praju0911 <vishalprajwal24@gmail.com>
2025-07-01 21:47:38 +0530

    Temporarily added db.create_all() to initialize PostgreSQL table

--- app.py
+++ app.py
@@ -55,4 +55,6 @@ def update(id):
         return render_template('update.html', task=task)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=8000)
